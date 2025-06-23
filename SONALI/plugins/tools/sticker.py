ee47d448a640f9eb28d20f319ba7546da2a9903f
VT7org <glitchnaxusofficial@gmail.com>
2025-06-23 22:54:17 +0530

    Update sticker.py

--- SONALI/plugins/tools/sticker.py
+++ SONALI/plugins/tools/sticker.py
@@ -30,7 +30,7 @@ def generate_sticker(client, message):
 
 
 
-@app.on_message(filters.command("kang"))
+@app.on_message(filters.command("packkang"))
 async def _packkang(app :app,message):  
     txt = await message.reply_text("**ᴘʀᴏᴄᴇssɪɴɢ....**")
     if not message.reply_to_message:
