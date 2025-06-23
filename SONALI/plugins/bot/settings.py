2afb26d95088be168ec65c72ed1355e27731ddd6
VT7org <glitchnaxusofficial@gmail.com>
2025-06-23 23:51:07 +0530

    Update settings.py

--- SONALI/plugins/bot/settings.py
+++ SONALI/plugins/bot/settings.py
@@ -54,7 +54,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://t.me/YukitaUpdates"),
+        InputMediaVideo("https://files.catbox.moe/69v8zn.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
